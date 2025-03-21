c8ed5ac52fe1f9acbca7e5bd2802a2555fb3d6ba
rippr-r <136972470+rippr-r@users.noreply.github.com>
2025-03-21 08:52:58 -0400

    Add files via upload

---
+++ OOP-Classwork/DayCareStatsTester.java
@@ -0,0 +1,28 @@
+public class DayCareStatsTester {
+    public static void main(String[] args){
+        DayCareStatistics dcs = new DayCareStatistics();
+ 
+         //Task 1
+         dcs.print_names();
+         System.out.println();
+ 
+         //Task 2
+         dcs.print_average_ages();
+         System.out.println();
+ 
+         //Task 3
+         dcs.histogram();
+         System.out.println();
+ 
+         //Task 4
+         dcs.print_revenue();
+         System.out.println();
+ 
+         //Task 5
+         dcs.child_lookup();
+         System.out.println();
+         
+         //Task 6 
+         dcs.print_last_names();     
+    }
+}
